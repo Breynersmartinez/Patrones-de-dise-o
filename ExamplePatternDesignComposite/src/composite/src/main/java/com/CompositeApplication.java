c6a486be8d3eb0762bf4243046ead7eb6df34906
Breiner Saul Martinez Muñoz <153401516+Breynersmartinez@users.noreply.github.com>
2025-09-24 14:59:00 +0000

    Ejericico composite terminado

---
+++ ExamplePatternDesignComposite/src/composite/src/main/java/com/CompositeApplication.java
@@ -0,0 +1,27 @@
+package com;
+
+
+public class CompositeApplication {
+public static void main(String args[])
+{
+// create de main department 
+Department salesDepartment = new SalesDepartment(1, "Sales Department");
+
+// create subdepartments 
+HeadDepartment headDepartment = new HeadDepartment(2, "Head Department");
+Department financialDepartment = new FinancialDepartment(3, "Financial Department");
+
+// print the department names
+headDepartment.addDepartment(salesDepartment);
+//
+headDepartment.addDepartment(financialDepartment);
+
+headDepartment.printDepartmentName();
+
+}
+}
+
+
+
+
+
