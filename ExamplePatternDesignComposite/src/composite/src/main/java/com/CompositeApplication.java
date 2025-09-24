b338735e2331c432402b0b5632dffd9c20aba7fd
Breiner Saul Martinez Muñoz <153401516+Breynersmartinez@users.noreply.github.com>
2025-09-24 10:25:56 -0500

    Se ajustaron losa argumentos de los departamentos

--- ExamplePatternDesignComposite/src/composite/src/main/java/com/CompositeApplication.java
+++ ExamplePatternDesignComposite/src/composite/src/main/java/com/CompositeApplication.java
@@ -5,11 +5,11 @@ public class CompositeApplication {
 public static void main(String args[])
 {
 // create de main department 
-Department salesDepartment = new SalesDepartment(1, "Sales Department");
+Department salesDepartment = new SalesDepartment(1, " Sales Department ");
 
 // create subdepartments 
-HeadDepartment headDepartment = new HeadDepartment(2, "Head Department");
-Department financialDepartment = new FinancialDepartment(3, "Financial Department");
+HeadDepartment headDepartment = new HeadDepartment(2, " Head Department ");
+Department financialDepartment = new FinancialDepartment(3, " Financial Department ");
 
 // print the department names
 headDepartment.addDepartment(salesDepartment);
